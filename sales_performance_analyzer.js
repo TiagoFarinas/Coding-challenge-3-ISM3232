3c413acb2394a1b531c02d7c5074be79ff805634
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-07 13:57:11 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -3,7 +3,16 @@
 function calculateAverageSales(sales_figures){
     if(sales_figures.length ===0) return 0
     var SalesTotal = sales_figures.reduce((acc, curr)=> acc + curr, 0)
-    return SalesTotal/sales_figures.length}
+    return SalesTotal/sales_figures.length};
     // This function will calculate the average sales for the company
+
+//Task 2-Create a Function to Determine Performance Rating
+var averageSales = SalesTotal/sales_figures.length;
+function determinePerformanceRating (averageSales){
+    if (averageSales>10000) {return "Execellent"}
+    else if (averageSales>7000 && averageSales<=1000) {return "Good"}
+    else if (averageSales>=4000 && averageSales<7000) {return "Satisfactory"}
+    else { return"Needs Improvement"}};
+// This function uses a series of if else statements to check in what bracket does the average sales for the company fits in and return a string with its "grade".
 
 
