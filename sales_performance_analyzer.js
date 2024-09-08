00a27e45421572f73ca0ebda4cf0b3d397dcf9f3
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-08 17:59:33 -0400

    Develop findTopAndBottomPerformers function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -15,4 +15,25 @@ function determinePerformanceRating (averageSales){
     else { return"Needs Improvement"}};
 // This function uses a series of if else statements to check in what bracket does the average sales for the company fits in and return a string with its "grade".
 
-
+//Task 3-Create a Function to Identify Top and Bottom Performers
+function calculateTotalSales(sales_figures) {
+    return sales_figures.reduce((total, figure) => total + figure, 0)}
+  function findTopAndBottomPerformers(salesData) {
+    if (salesData.length === 0) {
+      return { topPerformer: null, bottomPerformer: null } };
+  const topPerformer = salesData.reduce((top, current) => {
+      return calculateTotalSales(current.sales_figures) > calculateTotalSales(top.sales_figures) ? current : top });
+  const bottomPerformer = salesData.reduce((bottom, current) => {
+      return calculateTotalSales(current.sales_figures) < calculateTotalSales(bottom.sales_figures) ? current : bottom});
+  return {
+      topPerformer: {
+        name: topPerformer.name,
+        totalSales: calculateTotalSales(topPerformer.sales_figures),
+      },
+      bottomPerformer: {
+        name: bottomPerformer.name,
+        totalSales: calculateTotalSales(bottomPerformer.sales_figures),
+      }}}
+      // This part of the code will identify the best and worst performer among the sales team
+  
+  
